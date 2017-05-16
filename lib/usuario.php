871432dfaa7799bca883a2f5e372c5cd5fc7fdc1
cetecom <cetecom@LC08L5711>
2017-05-16 10:50:12 -0400

    ja ja ja ja

---
+++ lib/usuario.php
@@ -0,0 +1,16 @@
+<?php
+class Usuario{
+    var $idusuario;
+    var $nombre;
+    var $clave;
+    
+    function VerificaUsuario(){
+        
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
